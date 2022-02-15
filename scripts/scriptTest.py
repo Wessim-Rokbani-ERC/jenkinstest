49461d2724f6f3fd717c639f67c18db604cd885e
Wessim-Rokbani-ERC <wessim.rokbani@ericsson.com>
2022-02-15 18:02:57 -0500

    Create scriptTest.py

---
+++ scripts/scriptTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A simple Python script || rzfeeser@alta3.com"""
+
+
+def main():
+
+    # this will be displayed to standard out
+    print("Test done for Cert Project by Wessim Rokbani...")
+    
+if __name__ == "__main__":
+    main()
